d9218b946e62cc89d7d25bb6dbd64de7311caee2
Henry Z <hnryzhng@gmail.com>
2017-02-24 16:41:41 +0000

    connected succesfully to db

--- server.js
+++ server.js
@@ -7,32 +7,38 @@ var http = require('http');
 var express = require('express');
 var path = require('path');
 
-var mongo = require('mongodb').MongoCLient;
-var dburl = "mongodb://localhost:27017/urldb";
+var mongo = require('mongodb').MongoClient;
+var dburl = 'mongodb://localhost:27017/urldb';
 var dbmethods = require('./dbmethods.js'); 
+
+var shortenurl = require('./shortenurl.js');
 
 var app = express();
 
 // router: shorten specified url 
-app.get('/shorten/:targeturl(*)', function (request, response, next) {
+
+app.get('/:targeturl(*)', function (request, response, next) {
+  
+  var targeturl = request.params['targeturl'];
+  var shortenedurl = shortenurl.shorten(targeturl);
+  
+  var jsonObj = {
+    'original-url': targeturl,
+    'short-url': request.host + '/' + shortenedurl  
+  };
   
   // connect to db
   mongo.connect(dburl, function(err, db) {
-    if (err) {
-      throw err;
-    } else {
-      console.log('connected to db');
-      // else if check if url in db (collection.findOne()) then next(), if not then insert into db
-      // dbmethods.insert(db, targeturl);
-      db.close();
-    }
-    
+    if (err) throw err;
+    console.log('connected to db');
+    // else if check if url in db (collection.findOne()) then next(), if not then insert into db
+    dbmethods.insert(db, jsonObj);
+    db.close();
   });
   
-  var targeturl = request.params['targeturl'];
-  
+  response.write(targeturl);
   // response.json(jsonCollection);
-  response.end(targeturl);
+  response.end();
   
 });
 
